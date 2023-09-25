91f221acd597de7b58809069687e136bcf01aedf
SharonObange <obangesharon@gmail.com>
2023-09-25 12:10:36 -0700

    Handling Value Error exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        integer = "{:d}".format(value)
-        print(integer)
+        print("{:d}".format(value))
         return True
 
     except ValueError:
