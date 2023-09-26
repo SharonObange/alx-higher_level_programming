3f871f1ec38f1775808475f0905e5d65dd74b945
SharonObange <obangesharon@gmail.com>
2023-09-26 14:22:13 -0700

    Compare two squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+
+
+""" This class defines a square """
+
+
+class Square:
+    """
+    Attributes:
+        __size (int): private, an integer, length of each side
+
+    Methods:
+        None
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of Square with the given size
+
+        Args:
+            size (int, optional): length of the sides of the square
+                                set to 0 by default
+        Returns:
+            None
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the size of the square
+
+        Returns:
+            int: The length of each side of the square
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square
+
+        Args:
+            value (int): The length of each side of the square
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """
+        Initializes a new instance of Square by calculating its area
+
+        Args:
+            None
+
+        Returns:
+            The current Square area
+        """
+        area = self.__size * self.__size
+
+        return area
+
+    def __eq__(self, other):
+        """ Answers to comparator ="""
+        if isinstance(other, Square):
+            return self.area() == other.area()
+        return False
+
+    def __ne__(self, other):
+        """"Answers to comparator !="""
+        return not self.__eq__(other)
+
+    def __gt__(self, other):
+        """"Answers to comparator >"""
+        if isinstance(other, Square):
+            return self.area() > other.area()
+        return False
+
+    def __ge__(self, other):
+        """"Answers to comparator >="""
+        return self.__eq__(other) or self.__gt__(other)
+
+    def __lt__(self, other):
+        """"Answers to comparator <"""
+        if isinstance(other, Square):
+            return self.area() < other.area()
+        return False
+
+    def __le__(self, other):
+        """"Answers to comparator <="""
+        return self.__eq__(other) or self.__lt__(other)
