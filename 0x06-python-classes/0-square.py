3547b0380c3a5634b3c5a529c28080e7cb402f36
SharonObange <obangesharon@gmail.com>
2023-09-26 11:28:40 -0700

    Write an empty class that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This class is used to define a square.
+    A square is defined by its four equal sides and four right angles.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    	pass
