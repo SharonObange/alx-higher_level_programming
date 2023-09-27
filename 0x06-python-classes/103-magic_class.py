577d982ad40c891bd226f7e44a6d1884b71fc40c
SharonObange <obangesharon@gmail.com>
2023-09-26 22:15:04 -0700

    Update byteCode -> Python

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,14 +5,16 @@ import math
 
 
 class MagicClass:
-
+    """ Checks if radius is a number"""
     def __init__(self, radius=0):
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
+    """Calculates the area based on the given radius"""
     def area(self):
-        return self.__radius ** 2 * math.pi
+        return self.radius ** 2 * math.pi
 
+    """Claculates the circumference based ona given radius"""
     def circumference(self):
-        return 2 * math.pi * self.__radius
+        return 2 * math.pi * self.radius
