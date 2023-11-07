89557cc950abfb9e1cbad2cefabb3162fe9477cc
SharonObange <obangesharon@gmail.com>
2023-11-07 08:58:05 -0800

    Read file and print contents

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """
+    This module reads a text file (UTF8) and prints the content
+    Args:
+        filename
+    """
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line, end='')
+    except FileNotFoundError:
+        pass
