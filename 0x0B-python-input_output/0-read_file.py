64d1780743c0ffc87b261f783b06057a10862be4
SharonObange <obangesharon@gmail.com>
2023-11-07 09:06:01 -0800

    Update documentation

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,8 +4,12 @@
 def read_file(filename=""):
     """
     This module reads a text file (UTF8) and prints the content
+
     Args:
         filename
+
+    Returns:
+        none
     """
     try:
         with open(filename, 'r', encoding='utf-8') as file:
