c60d95059b033a7fd7e9cb1b5b7cffbcfd0f5c62
SharonObange <obangesharon@gmail.com>
2023-11-07 10:43:47 -0800

    Convert to JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """ This illustrates how to return the JSON rep of an object"""
 
 
+import json
 def to_json_string(my_obj):
     """ Return JSON rep"""
     return json.dumps(my_obj)
